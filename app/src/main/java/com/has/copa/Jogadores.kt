abd5033221213ef8ef234920fc3217f9477bed8e
carloedu11 <miguelodoom22@hotmail.com>
2022-11-29 22:38:49 -0300

    Copa

---
+++ app/src/main/java/com/has/copa/Jogadores.kt
@@ -0,0 +1,12 @@
+package com.has.copa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Jogadores : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_jogadores)
+    }
+
+}
